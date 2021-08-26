eebf916f8d5e446f3e31815086ae80b7ccdfbceb
Nico Tolosa <nicolas.tolosagonzalez@gmail.com>
2021-08-26 10:50:28 +0200

    Refactored the code to read csv files from kaggle competitions API. Introduced utils.kaggle.py module, with several methods and classes to pack all the functionallity and abstract it from the AirBnb.source implementation.

--- AirBnbModel/source/run.py
+++ AirBnbModel/source/run.py
@@ -9,13 +9,11 @@ Configuration parameters for this task can be found on configs/config.yml
 """
 import logging
 import os
-import shutil
-import zipfile
 
-import kaggle
 import pandas as pd
 
 from AirBnbModel.config import cfg
+from AirBnbModel.utils import kaggle
 
 logger = logging.getLogger(__name__)
 
@@ -27,55 +25,19 @@ def train_eval():
     This task extracts different tables containing training data and the target
     variable and loads it into a staging database, prior to its preprocessing.
     """
-    # Create connection and authenticate to kaggle API
-    kaggle_api = kaggle.KaggleApi()
-    kaggle_api.authenticate()
 
-    COMPETITION_NAME = "airbnb-recruiting-new-user-bookings"
-    TEMP_DESTINATION_PATH = "data/train/source/temp"
-    try:
-        # download_data
-        os.makedirs(TEMP_DESTINATION_PATH, exist_ok=True)
-        kaggle_api.competition_download_files(
-            competition=COMPETITION_NAME, path=TEMP_DESTINATION_PATH
-        )
-        logger.info(f"Data downloaded in {TEMP_DESTINATION_PATH}")
+    # Variable declaration
+    COMPETITION_NAME: str = "airbnb-recruiting-new-user-bookings"
+    DESTINATION_PATH: str = "data/train/source"
 
-        # unzip_data
-        ZIP_PATH = os.path.join(TEMP_DESTINATION_PATH, COMPETITION_NAME + ".zip")
-        training_datasets = ["train_users_2.csv.zip", "sessions.csv.zip"]
-        with zipfile.ZipFile(ZIP_PATH, "r") as zip_ref:
-            zip_ref.extractall(TEMP_DESTINATION_PATH, training_datasets)
-        os.remove(ZIP_PATH)  # Remove .zip after extracting its contents
-        logger.info("main zip file extracted")
+    users: pd.DataFrame
+    sessions: pd.DataFrame
 
-        for dataset in training_datasets:
-            zip_path = os.path.join(TEMP_DESTINATION_PATH, str(dataset))
+    # Extract data from source
+    source = kaggle.CompetitionAPI(competition_name=COMPETITION_NAME)
 
-            with zipfile.ZipFile(zip_path, "r") as zip_ref:
-                zip_ref.extractall(TEMP_DESTINATION_PATH)
-
-            os.remove(zip_path)
-            logger.info(f"{dataset} extracted in {TEMP_DESTINATION_PATH}")
-
-        # Read .csv files
-        users_path = os.path.join(TEMP_DESTINATION_PATH, "train_users_2.csv")
-        users = pd.read_csv(users_path)
-        logger.info("users dataset loaded")
-
-        sessions_path = os.path.join(TEMP_DESTINATION_PATH, "sessions.csv")
-        sessions = pd.read_csv(sessions_path)
-        logger.info("sessions dataset loaded")
-
-    except Exception as e:
-        logger.error(f"ERROR: {e}")
-
-    finally:
-        try:
-            shutil.rmtree(TEMP_DESTINATION_PATH)
-            logger.info(f"Temporary folder {TEMP_DESTINATION_PATH} deleted")
-        except OSError as e:
-            print("Error: %s : %s" % (TEMP_DESTINATION_PATH, e.strerror))
+    users = source.read_csv(filename="train_users_2.csv")
+    sessions = source.read_csv(filename="sessions.csv")
 
     # sessions.get_unique_IDs()
 
@@ -85,36 +47,12 @@ def train_eval():
 
     # train_test_split()
 
-    # store_data()
-    DESTINATION_PATH = "data/train/source"
+    # store_data
+    users_destination = os.path.join(DESTINATION_PATH, "users.csv")
+    users.to_csv(users_destination, index=False)
 
-    users_destination_path = os.path.join(DESTINATION_PATH, "users.csv")
-    users.to_csv(users_destination_path, index=False)
-    logger.info(f"users dataset stored in {users_destination_path}")
-
-    sessions_destination_path = os.path.join(DESTINATION_PATH, "sessions.csv")
-    sessions.to_csv(sessions_destination_path, index=False)
-    logger.info(f"sessions dataset stored in {sessions_destination_path}")
-
-    # df: pd.DataFrame
-    # destination: str
-
-    # # parameters to pass pd.to read_csv().
-    # source_params: dict = cfg.source.data.train_eval
-    # for dataset in source_params:
-
-    #     df = pd.read_csv(**source_params[dataset])
-
-    #     logger.info(
-    #         f"""{df.shape[0]} rows and {df.shape[1]} colums loaded for
-    #         {dataset} from {source_params[dataset]['filepath_or_buffer']}"""
-    #     )
-    #     logger.debug(f"{dataset} dataset structure preview:\n{df.head(2)}")
-
-    #     destination = cfg.source.params.destination.train_eval[dataset]
-    #     df.to_pickle(destination)
-
-    #     logger.info(f"{dataset} dataset saved on {destination}")
+    sessions_destination = os.path.join(DESTINATION_PATH, "sessions.csv")
+    sessions.to_csv(sessions_destination, index=False)
 
 
 def predict():
---
+++ AirBnbModel/utils/kaggle.py
@@ -0,0 +1,170 @@
+"""
+Contains classes and methods needed to connect to Kaggle API and extract data
+from it.
+
+See also
+----------
+
+See https://www.kaggle.com/docs/api#authentication for details on the
+authentication requierements to use the API.
+
+"""
+
+import logging
+import os
+import tempfile
+import zipfile
+
+import pandas as pd
+
+
+# kaggle creates a connection and validates authentication on import. If
+# import fails due to an authentication error, AuthenticationError is raised.
+class AuthenticationError(Exception):
+    """Exception to handle authentication failure on Kaggle API."""
+
+    pass
+
+
+try:
+    import kaggle
+    from kaggle.rest import ApiException
+except OSError as e:
+    if str(e).find("Could not find kaggle.json") != -1:
+        raise AuthenticationError(
+            f"{e} See https://www.kaggle.com/docs/api#authentication for details on the authentication requierements to use the API."
+        ) from None
+    else:
+        raise
+
+logger = logging.getLogger(__name__)
+
+
+class CompetitionAPI:
+    """
+    Object that holds a connection to the Kaggle API with several methods to
+    retreive data about competitions.
+
+    Parameters
+    ----------
+    competition_name : str
+        name of the Kaggle competition to which the connection will be
+        stablished. competition_name can be extracted from the URL of the
+        homepage of the competition kaggle.com/c/competition_name
+    """
+
+    def __init__(self, competition_name: str):
+        self.api = kaggle.KaggleApi()
+        self.api.authenticate()
+        self.competition_name = competition_name
+
+    def read_csv(self, filename: str) -> pd.DataFrame:
+        """
+        Reads comma-separated values (csv) dataset from Kaggle competition
+        into a pandas DataFrame.
+
+        Parameters
+        ----------
+        filename
+            name of the .csv file to return. Se
+            :func:`~CompetitionAPI.download_dataset` method's documentation
+            for specifications on the format.
+
+        Returns
+        ----------
+        pandas.DataFrame
+            Returns the selected dataset of the competition as a DataFrame.
+
+        Raises
+        ----------
+        AssertionError
+            If filename has not a .csv extension.
+
+        Examples
+        --------
+        returns sessions.csv dataset from Kaggle competition
+        airbnb-recruiting-new-user-bookings, as a pandas DataFrame.
+
+        >>> competition = CompetitionAPI('airbnb-recruiting-new-user-bookings')
+        >>> competition.read_csv('sessions.csv')
+
+        """
+
+        assert (
+            os.path.splitext(filename)[-1] == ".csv"
+        ), f"{filename} is not a .csv file"
+        zipped_filename = filename + ".zip"
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            # Download file using Kaggle API
+            self.download_dataset(path=tmpdir, filename=zipped_filename)
+
+            # Unpack .zip to extract .csv file
+            zipped_filepath = os.path.join(tmpdir, zipped_filename)
+            with zipfile.ZipFile(zipped_filepath, "r") as zip:
+                zip.extractall(tmpdir)
+
+            # read csv
+            csv_filepath = os.path.join(tmpdir, filename)
+            df = pd.read_csv(csv_filepath)
+
+        return df
+
+    def download_dataset(self, path: str, filename: str):
+        """
+        Downloads kaggle competition dataset specifyed in filename into path.
+
+        Parameters
+        ----------
+        path
+            relative or absolute path of the directory where downloaded
+            dataset should be stored.
+        filename
+            name of the file to return. It must be one of the names
+            returned by :func:`~CompetitionAPI.dataset_list` method.
+
+        Raises
+        ----------
+        FileNotFoundError :
+            If the dataset passed was not found in Kaggle API.
+
+        Examples
+        --------
+        downloads sessions.csv.zip dataset from Kaggle competition
+        airbnb-recruiting-new-user-bookings into documents/ directory.
+
+        >>> competition = CompetitionAPI('airbnb-recruiting-new-user-bookings')
+        >>> competition.read_csv('documents/', 'sessions.csv.zip')
+
+        """
+        try:
+            self.api.competition_download_file(
+                competition=self.competition_name,
+                path=path,
+                file_name=filename,
+                quiet=True,
+            )
+
+        except ApiException as e:
+            error_msg = (
+                f"Unable to find {filename} in {self.competition_name} competition"
+            )
+            logger.exception(e)
+            raise FileNotFoundError(error_msg)
+
+        logger.info(
+            f"{filename} succesfully downloaded from {self.competition_name} Kaggle competition"
+        )
+
+    def dataset_list(self) -> list[str]:
+        """
+        Lists all the datasets available to be accessed with Kaggle API for
+        a certain competition.
+
+        Returns
+        ----------
+        list of str
+            Available datasets for the kaggle competition.
+
+        """
+        return self.api.competition_list_files(self.competition)
